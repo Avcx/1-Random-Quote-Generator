0bc3e3894a2a81175061df3026171ebc03bb8e6e
Akiel Adams <avcx12@gmail.com>
2023-03-03 22:01:38 -0700

    Added quote array and main functions declarations.

--- js/script.js
+++ js/script.js
@@ -3,27 +3,63 @@ Treehouse FSJS Techdegree:
 project 1 - A Random Quote Generator
 ******************************************/
 
-// For assistance: 
+// For assistance:
   // Check the "Project Resources" section of the project instructions
   // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
 
-/*** 
- * `quotes` array 
+/***
+ * `quotes` array
 ***/
 
+const quotes = [
 
+  {
+    quote: '',
+    source: '',
+    citation: ''
+  },
+
+  {
+    quote: '',
+    source: '',
+    year: ''
+  },
+
+  {
+    quote: '',
+    source: ''
+  },
+
+  {
+    quote: '',
+    source: ''
+  },
+
+  {
+    quote: '',
+    source: ''
+  },
+];
 
 /***
  * `getRandomQuote` function
 ***/
 
+function getRandomQuote() {
 
+  let quoteNumber = Math.floor( Math.random() * 5 ) + 1;
+  
+}
 
 /***
  * `printQuote` function
 ***/
 
+function printQuote() {
 
+}
+
+getRandomQuote()
 
 /***
  * click event listener for the print quote button
